c3187cf0c7919747deb9cf85e148b1c085a1fca3
delavock@gmail.com <jp418362@students.mimuw.edu.pl>
2020-06-27 16:13:04 +0200

    deleted set coverage

--- Set Coverage/cover/Składnik.java
+++
@@ -1,8 +0,0 @@
-package cover;
-
-// z podklas tego elementu składa się cały zbiór
-// jest nadklasą elementu, ciągu ograniczonego oraz ciągu nieograniczonego
-public abstract class Składnik {
-    // sprawdza czy zadana wartość istnieje w którejś z podklas
-    abstract boolean czyIstnieje(int wartość);
-}
